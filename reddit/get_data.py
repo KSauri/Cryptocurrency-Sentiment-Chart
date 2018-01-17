d4f02416099e617e6dee8d44e7558b2be4ee14f2
Kyle Sauri <kylesauri@gmail.com>
2018-01-16 20:19:24 -0500

    add reddit tasks

---
+++ reddit/get_data.py
@@ -0,0 +1,63 @@
+import json
+import os
+import praw
+import datetime
+
+from functools import reduce
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()
+
+reddit = praw.Reddit(
+                     client_id=os.environ.get('REDDIT_ID'),
+                     client_secret=os.environ.get('REDDIT_SECRET'),
+                     password=os.environ.get('REDDIT_PASSWORD'),
+                     user_agent=os.environ.get('REDDIT_USER_AGENT'),
+                     username=os.environ.get('REDDIT_USERNAME')
+                     )
+
+# TODO introduce logging
+
+
+class Reddit:
+    """
+    The Reddit class pulls streaming Reddit comments and assigns each comment
+    a sentiment score.
+
+    The `get_data()` method returns a generator that provides access to streaming Reddit comments
+
+    The `process_data()` method uses the instance variable `self.timestamp` and will return
+    the average sentiment of a five minute interval starting with `self.timestamp` of all Reddit comments.
+    """
+
+    def __init__(self, timestamp):
+        delta = datetime.timedelta(seconds=300)
+        self.timestamp = timestamp
+        self.finish_time = timestamp + delta
+
+    def get_data(self):
+        """
+        TODO add docstring
+        """
+        for comment in reddit.subreddit('all').stream.comments():
+            yield comment.body
+
+    def _get_score(self, reddit_comment):
+        return sid.polarity_scores(reddit_comment)['compound']
+
+    def process_data(self):
+        final_sentiment_data = {
+            'avg_sentiment': 0.0,
+            'timestamp': self.timestamp
+        }
+
+        sentiments = []
+        comments = self.get_data()
+
+        # NB while loop = super gross here
+        while datetime.datetime.now() < self.finish_time:
+            next_comment = next(comments)
+            sentiments.append(self._get_score(next_comment))
+
+        final_sentiment_data['avg_sentiment'] = reduce(lambda x, y: x + y, sentiments) / len(sentiments)
+        return json.dumps(final_sentiment_data)
