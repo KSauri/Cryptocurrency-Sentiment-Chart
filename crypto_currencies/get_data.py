f7c82c91c0205a69f0680a6d302ae21dfb6e7f7e
Kyle Sauri <kylesauri@gmail.com>
2018-01-16 20:18:40 -0500

    update crypto tasks

--- crypto_currencies/get_data.py
+++ crypto_currencies/get_data.py
@@ -1,29 +1,36 @@
-import datetime
 import json
 import requests
 
 from ast import literal_eval
 
-# TODO obviously fix all this
-
-# One function to get the list of all currencies
-# And another that uses 'sched' to make a call every 3 minutes
-# How do I keep track of the last time that coin's price was called?
-
 
 class CryptoCurrency:
+    """
+    CryptoCurrency relies on an API that returns the market cap for various crypto currencies
+
+    The `get_data()` method returns data, including the market cap and current price, for the 25 largest
+    crypto currencies
+
+    The `process_data()` method returns a json string with data on the overall market cap
+    as well as per coin data on price, rank, and market cap in USD
+    """
+
+    def __init__(self, timestamp):
+        # TODO add check for timestamp type
+        self.timestamp = timestamp
 
     def get_data(self):
         return requests.get('https://api.coinmarketcap.com/v1/ticker/?limit=25').content
 
-    def process_data(self, crypto_data_response, timestamp):
+    def process_data(self, crypto_data_response):
         price_data = crypto_data_response.decode()
         price_data = price_data.replace('null', 'None')
         price_data = literal_eval(price_data)
 
         final_price_data = {
             'market_cap': 0.0,
-            'coins': []
+            'coins': [],
+            'timestamp': self.timestamp
         }
         for coin in price_data:
             final_price_data['market_cap'] += float(coin['market_cap_usd'])
@@ -34,4 +41,4 @@ class CryptoCurrency:
                 'market_cap': coin['market_cap_usd']
             })
 
-        return final_price_data
+        return json.dumps(final_price_data)
